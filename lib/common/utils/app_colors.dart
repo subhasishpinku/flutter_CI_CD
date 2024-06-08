8fd5e59c1bc609d417031ef49ca5a3ec8ab0529d
subhasishpinku <pinku.subhasish@gmail.com>
2024-06-08 14:49:28 +0530

    first commit

---
+++ lib/common/utils/app_colors.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+class AppColors {
+
+   int x= 0;
+  /// white background
+  static const Color primaryBackground = Color.fromARGB(255, 255, 255, 255);
+  /// grey background
+  static const Color primarySecondaryBackground = Color.fromARGB(255, 247, 247, 249);
+  /// main widget color blue
+  static const Color primaryElement = Color.fromARGB(255, 61, 61, 216);
+  /// main text color black
+  static const Color primaryText = Color.fromARGB(255, 0, 0, 0);
+  // video background color
+  static const Color primaryBg = Color.fromARGB(210, 30, 45, 61);
+  /// main widget text color white
+  static const Color primaryElementText = Color.fromARGB(255, 255, 255, 255);
+  // main widget text color grey
+  static const Color primarySecondaryElementText = Color.fromARGB(255, 102, 102, 102);
+  // main widget third color grey
+  static const Color primaryThirdElementText = Color.fromARGB(255, 175, 170, 170);
+
+  static const Color primaryFourthElementText = Color.fromARGB(255, 204, 204, 204);
+  //state color
+  static const Color primaryElementStatus = Color.fromARGB(255, 77, 168, 118);
+
+  static const Color primaryElementBg = Color.fromARGB(255, 231, 93, 69);
+
+
+}
