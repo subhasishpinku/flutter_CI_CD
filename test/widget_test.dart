817fdfb31f123eb1d763598bd5a0558554fc4a31
subhasishpinku <pinku.subhasish@gmail.com>
2024-06-14 11:42:06 +0530

    build_upload_notifysss

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ulearning_flutter_riverpod/main.dart'; // Correct the path as needed
 
 void main() {
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp()); // Ensure MyApp is a valid class
-    // Your test code here
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
